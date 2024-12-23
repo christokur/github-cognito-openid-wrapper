115787b8587f73f06383be70d814106673ac0ec7
Christo De Lange <christo.delange@sands.com>
2024-12-23 16:30:45 -0500

    fix: coverage

---
+++ src/connectors/lambda/jwks.test.js
@@ -0,0 +1,44 @@
+const { mockAxios } = require('../../sharedMocks');
+const { mockValues } = require('../../mocks');
+
+let controllers;
+let jwks;
+
+describe('Lambda JWKS Handler', () => {
+  beforeEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+    jest.mock('../controllers', () => jest.fn(() => ({ jwks: jest.fn() })));
+    controllers = require('../controllers');
+    jwks = require('./jwks');
+  });
+
+  afterEach(() => {
+    jest.resetModules();
+    delete require.cache[require.resolve('../controllers')];
+    delete require.cache[require.resolve('./jwks')];
+  });
+
+  test('should return JWKS', () => {
+    const mockResponse = {
+      keys: [
+        {
+          kty: 'RSA',
+          kid: 'test-key-id',
+          use: 'sig',
+          alg: 'RS256',
+          n: 'test-modulus',
+          e: 'AQAB'
+        }
+      ]
+    };
+
+    const mockJwks = jest.fn().mockReturnValue(mockResponse);
+    controllers.mockReturnValue({ jwks: mockJwks });
+
+    const result = jwks.handler();
+
+    expect(mockJwks).toHaveBeenCalled();
+    expect(result).toEqual(mockResponse);
+  });
+});
